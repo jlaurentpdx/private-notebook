59669c0fb4983ca7beaa84b2d0a0ce2e93350a03
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-23 15:06:48 -0800

    forcing Netlify update

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,8 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
 
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
 export default function PrivateRoute({ children, ...rest }) {
   const { user } = useUser();
 
